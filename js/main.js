511f08ecce49e1ec56dba47182a4cf54f7a86c0d
Lucas <derechosreservados.inc@gmail.com>
2018-03-15 23:21:36 -0300

    0.2
    No se en que versión estaba, no importa. Lo que importa es el presente: no anda pero anda el keypad. Escape borra todo y Enter apreta el botón de ingreso.

--- js/main.js
+++ js/main.js
@@ -1,52 +1,109 @@
 $(document).ready(() => {
-    const $button = $('.button');
+    const $button = $('#ingresar');
     const $right = $('.right');
-    const $monto = $('#monto');
-    window.onkeydown = function(event) {
-        document.getElementById("monto").value;
-        if (monto <= 0 || ""){
-            $button.prop("disabled", true);
-        } else {$button.removeAttr('disabled')}
-        
-    $button.on('click', () => {
-        $right.slideDown(1000);
-        var monto = document.getElementById("monto").value;
-        var tarjeta = document.getElementById("tarjetaCredito").value;
-        var cuotas = document.getElementById("numCuotas").value;
-        var montoVisa = monto*1.25;
-        var montoMaster = monto*1.22;
-        var cuotasVisa = montoVisa/cuotas;
-        var cuotasMaster = montoMaster/cuotas;
-        cuotasVisa = cuotasVisa.toFixed(2);
-        cuotasMaster = cuotasMaster.toFixed(2);
-        if (tarjeta == "visa") {
-            document.getElementById("mtf").innerHTML = "$" + montoVisa;
-            document.getElementById("mcf").innerHTML = "$" + cuotasVisa;
-        }
-        if (tarjeta == "master") {
-            document.getElementById("mtf").innerHTML = "$" + montoMaster;
-            document.getElementById("mcf").innerHTML = "$" + cuotasMaster; 
-        }
-        if (tarjeta == "") {
-            document.getElementById("mtf").innerHTML = "Especifique tarjeta";
-            document.getElementById("mcf").innerHTML = "Error";     
-        }
-        if (cuotas == "") {
-            document.getElementById("mtf").innerHTML = "Error";
-            document.getElementById("mcf").innerHTML = "Especifique cuotas"; 
-        }
-        if (tarjeta == "" && cuotas == "") {
-            document.getElementById("mtf").innerHTML = "Especifique tarjeta";
-            document.getElementById("mcf").innerHTML = "Especifique cuotas";  
-        }
-    })
+    const $pass = $('#pass');
+    const $uno = $('#uno');
+    const $dos = $('#dos');
+    const $tres = $('#tres');
+    const $cuatro = $('#cuatro');
+    const $cinco = $('#cinco');
+    const $seis = $('#seis');
+    const $siete = $('#siete');
+    const $ocho = $('#ocho');
+    const $nueve = $('#nueve');
+    const $cero = $('#cero');
+    var password = document.getElementById('pass').value;
     
-    if ( event.keyCode == 13 ) {
-        document.getElementsByTagName('button')[0].click();
-    }
-    if ( event.keyCode == 27 ) {
-        $right.slideUp(1000);
-        $monto.val("");
-        document.forms["calcuMon"].reset();
-    }};
+    $uno.on('click', () => {
+        password = password + "1";
+        $pass.val(password);
+    })
+    $dos.on('click', () => {
+        password = password + "2";
+        $pass.val(password);
+    })
+    $tres.on('click', () => {
+        password = password + "3";
+        $pass.val(password);
+    })
+    $cuatro.on('click', () => {
+        password = password + "4";
+        $pass.val(password);
+    })
+    $cinco.on('click', () => {
+        password = password + "5";
+        $pass.val(password);
+    })
+    $seis.on('click', () => {
+        password = password + "6";
+        $pass.val(password);
+    })
+    $siete.on('click', () => {
+        password = password + "7";
+        $pass.val(password);
+    })
+    $ocho.on('click', () => {
+        password = password + "8";
+        $pass.val(password);
+    })
+    $nueve.on('click', () => {
+        password = password + "9";
+        $pass.val(password);
+    })
+    $cero.on('click', () => {
+        password = password + "0";
+        $pass.val(password);
+    })
+
+    window.onkeydown = function(event) {
+        if ( event.keyCode == 13 ) {
+            $right.slideDown(1000); 
+            document.getElementById('ingresar').click();
+        }
+        if ( event.keyCode == 27 ) {
+            $right.slideUp(1000);
+            $pass.val("");
+            password = "";
+        }
+        if ( event.keyCode == 49 ) {
+            $right.slideUp(1000);
+            document.getElementById('uno').click();
+        }
+        if ( event.keyCode == 50 ) {
+            $right.slideUp(1000);
+            document.getElementById('dos').click();
+        }
+        if ( event.keyCode == 51 ) {
+            $right.slideUp(1000);
+            document.getElementById('tres').click();
+        }
+        if ( event.keyCode == 52 ) {
+            $right.slideUp(1000);
+            document.getElementById('cuatro').click();
+        }
+        if ( event.keyCode == 53 ) {
+            $right.slideUp(1000);
+            document.getElementById('cinco').click();
+        }
+        if ( event.keyCode == 54 ) {
+            $right.slideUp(1000);
+            document.getElementById('seis').click();
+        }
+        if ( event.keyCode == 55 ) {
+            $right.slideUp(1000);
+            document.getElementById('siete').click();
+        }
+        if ( event.keyCode == 56 ) {
+            $right.slideUp(1000);
+            document.getElementById('ocho').click();
+        }
+        if ( event.keyCode == 57 ) {
+            $right.slideUp(1000);
+            document.getElementById('nueve').click();
+        }
+        if ( event.keyCode == 48 ) {
+            $right.slideUp(1000);
+            document.getElementById('cero').click();
+        }
+    };
 })
